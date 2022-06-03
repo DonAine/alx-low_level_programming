1c28e21069811282877cc3be66e04b64690913fe
DonAine <30406256+DonAine@users.noreply.github.com>
2022-06-03 00:47:27 -0700

    Hello_puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-/*
+/**
  *main-Uses puts function to print out a string 
  *
  *Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is liked building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }

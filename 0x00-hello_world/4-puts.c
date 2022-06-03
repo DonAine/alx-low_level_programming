f29d120254345a8283eca22f81baa9ca491ca152
DonAine <30406256+DonAine@users.noreply.github.com>
2022-06-03 00:20:37 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ *main-Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is liked building a multilingual puzzle");
+	return (0);
+}

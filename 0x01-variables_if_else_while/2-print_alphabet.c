c0bb21451e58e982843f90b5f06ec462aa2fb560
DonAine <30406256+DonAine@users.noreply.github.com>
2022-06-03 16:05:41 -0700

    Lowercase alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints lowercase alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char alph = 'a';
+while (alph <= 'z'){
+	putchar(alph);
+	alph++;
+}
+putchar('\n');
+return (0);
+}

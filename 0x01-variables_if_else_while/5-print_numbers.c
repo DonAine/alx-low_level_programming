fa55e68672bdf9fc503bd8af76080a2c6ca484b3
DonAine <30406256+DonAine@users.noreply.github.com>
2022-06-03 19:19:00 -0700

    Solid singles

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,13 +2,12 @@
 /**
  * main - prints single base 10 digits starting from 0 
  * Return: Always 0 (Success)
- *
- *
  */
 
 int main(void)
 {
 	int c = 0;
+
 	while (c < 10)
 	{
 		printf("%d", c);

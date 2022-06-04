9dca99a7e610634351f2acf450bfaae19fc017b8
DonAine <30406256+DonAine@users.noreply.github.com>
2022-06-03 19:13:20 -0700

    Pos_or_negs

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,6 +5,7 @@
 
 /**
  * main- uses if statement to output negative, positive or zero
+ *
  * Return: Always 0 (Success) 
  */
 
@@ -21,7 +22,6 @@ else if (n == 0)
 	printf("%d is zero\n", n);
 else 
 	printf("%d is negative\n", n);
-
 return (0);
 
 }

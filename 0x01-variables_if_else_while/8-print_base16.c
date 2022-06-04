11e3f07e8db8b88d8ff5434f900b7519571c4c2b
DonAine <30406256+DonAine@users.noreply.github.com>
2022-06-03 19:50:42 -0700

    Hexa

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - prints hexadecimal
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alp_16 = 'a';
+	int num_16 = 0;
+
+	while (num_16 < 10)
+	{
+		putchar((num_16 % 10) + '0');                                                                                   
+	       	num_16++;                  
+	}
+	while (alp_16 < 'g')
+	{
+		putchar(alp_16);
+		alp_16++;
+	}
+putchar('\n');
+return (0);
+}

6d4889cd2f9d5a7a8688b76dad2cbdfa85c3b11f
DonAine <30406256+DonAine@users.noreply.github.com>
2022-06-03 17:57:02 -0700

    Single base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints single base 10 digits starting from 0 
+ * Return: Always 0 (Success)
+ *
+ *
+ */
+
+int main(void)
+{
+	int c = 0;
+	while (c < 10)
+	{
+		printf("%d", c);
+		c++;
+	}
+printf("\n");
+return (0);
+}

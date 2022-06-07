6b9525c31ee165396bcac8e482a0cbb120782fae
DonAine <30406256+DonAine@users.noreply.github.com>
2022-06-07 06:11:20 -0700

    ISlower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _islower - ascertains if a value is lowercase or not
+ * Return: 1(True) and 0(False)
+ * @c - argument for _islower computing
+ */
+
+int _islower(int c)
+{
+	if (c <= 122 && c >= 97)
+		return (1);
+	else
+	       	return (0);
+}

76e69e511ebefae9bd1d03f1973a2d5bf16a40dc
DonAine <30406256+DonAine@users.noreply.github.com>
2022-06-07 14:03:34 -0700

    Absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - outputs the absolute value
+ * @x: input argument
+ * Return: absolute value of value x
+ */
+
+int _abs(int x)
+{
+	if (x > 0)
+		return (x);
+	else
+		return (-x);
+}

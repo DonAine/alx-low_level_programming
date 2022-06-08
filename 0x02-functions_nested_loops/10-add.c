619845ee9d475134199e46c7fe49a56e66e23f3c
DonAine <30406256+DonAine@users.noreply.github.com>
2022-06-07 19:23:38 -0700

    Sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - outputs sum of 2 numbers
+ * Return: addition of the two input args
+ * @a: input arg
+ * @b: input arg
+ */
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}

85996bbef8734a193bbf31f778a8f202ad73bc1e
DonAine <30406256+DonAine@users.noreply.github.com>
2022-06-09 08:24:45 -0700

    Upper-1

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,12 +3,12 @@
 /**
  * _isupper - identifies whether input is uppercase
  * Return: 1 when true and 0 otherwise
- *
+ * @c: input variable
  */
 
 int _isupper(int c)
 {
-	if(c >= 'A' && c <= 'Z')
+	if (c >= 'A' && c <= 'Z')
 		return (1);
 	else
 		return (0);

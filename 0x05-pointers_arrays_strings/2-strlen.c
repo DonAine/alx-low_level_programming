9575dc99748664cafec8b46fe9026395f2924557
DonAine <30406256+DonAine@users.noreply.github.com>
2022-06-13 07:39:04 -0700

    String length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: input string
+ * Return: length of the string
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s++)
+		len++;
+	return (len);
+}

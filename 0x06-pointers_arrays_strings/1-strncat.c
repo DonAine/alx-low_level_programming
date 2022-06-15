3e51b71378dd531e44cc0f7312295939fdeebd73
DonAine <30406256+DonAine@users.noreply.github.com>
2022-06-15 10:17:39 -0700

    String Concat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strncat - concatenates two strings using at most number of bytes
+ * @dest: string to be concatenated upon
+ * @src: string to be appended to dest
+ * @n: specified number of bytes
+ * Return: a pointer to string dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
+	return (dest);
+}

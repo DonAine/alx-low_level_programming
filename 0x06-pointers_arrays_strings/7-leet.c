2490faacce5563b5b58a8180a1588457b5ada63a
DonAine <30406256+DonAine@users.noreply.github.com>
2022-06-16 02:54:42 -0700

    1337-correction

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,15 +8,16 @@
 char *leet(char *str)
 {
 	int indx1 = 0, indx2;
-	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
+	char leet[9] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T', '?'};
 
-	while (str[indx1++])
+	while (str[indx1])
 	{
-		for (indx2 = 0; indx2 < 7; indx2++)
+		for (indx2 = 0; indx2 < 8; indx2++)
 		{
 			if (str[indx1] == leet[indx2] || str[indx1] - 32 == leet[indx2])
 				str[indx1] = indx2 + '0';
 		}
+		indx1++;
 	}
 	return (str);
 
